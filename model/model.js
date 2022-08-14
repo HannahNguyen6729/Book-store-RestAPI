3f9057da7f3b6c8e87b7fcb0d54f8f7ee43caebc
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2022-08-14 16:49:34 +0300

    create module/schema

--- model/model.js
+++ model/model.js
@@ -1,1 +1,35 @@
 //create a structure for the database
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  publishedDate: {
+    type: String,
+  },
+  genres: {
+    type: [String],
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Author",
+  },
+});
+
+const authorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+  },
+  books: [{ type: mongoose.Schema.Types.ObjectId, ref: "Book" }],
+});
+
+const Author = mongoose.model("Author", authorSchema);
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = { Book, Author };
